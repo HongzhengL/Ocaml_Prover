bfc2785138ceb14cac25fa7e167630da8033d106
Hongzheng Li <roymanclee@gmail.com>
2023-12-12 23:04:37 -0600

    IH not yet support && proof process needs refactor

--- lib/rewrite.ml
+++ lib/rewrite.ml
@@ -9,11 +9,6 @@ let parse (s : string) : Ast.expr =
             ast
 
 
-module type Substitution = sig
-
-end
-
-
 module Substitution = struct
     module MM = Map.Make(String)
     type expression = Ast.expr
@@ -143,17 +138,36 @@ let rec performSteps (expr : Ast.expr) (equalities : (string * Ast.expr list opt
     | None -> []
        
     
-let containsDefinition (s : string) = 
+let isDefinition (s : string) = 
     try
-      Str.search_forward (Str.regexp_string "definition") s 0 |> ignore;
-      true
+        Str.search_forward (Str.regexp "^definition") s 0 |> ignore;
+        true
     with Not_found -> false
-
-
-let rec printSteps (steps : (string * Ast.expr) list) : string list =
+ 
+let split_at_last_pipe (s : string) =
+    try
+        let pos = String.rindex s '*' in
+        let before = String.sub s 0 pos in
+        let after = String.sub s (pos + 1) (String.length s - pos - 1) in
+        Some (before, after)
+    with Not_found ->
+        None
+ 
+let rec printStepsWithDefinitions (steps : (string * Ast.expr) list) : string list =
     match steps with
     | (name, expr) :: t -> 
-        ("= { " ^ name ^ " }\n" ^ Print.string_of_expr expr ^ "\n") :: printSteps t
+        if isDefinition name then
+            ("= { " ^ name ^ " }\n" 
+            ^ 
+            (* "= { matching " ^ " with " ^ " }\n"  *)
+            (* ^  *)
+            Print.string_of_expr expr ^ "\n") 
+            :: printStepsWithDefinitions t
+        else
+            ("= { " ^ name ^ " }\n" 
+            ^ 
+            Print.string_of_expr expr ^ "\n") 
+            :: printStepsWithDefinitions t
     | [] -> []
     
 
@@ -173,24 +187,78 @@ let produceProof (equality : Ast.expr) (equalities : (string * Ast.expr list opt
         let steps_non_empty = List.length left_steps > 0 && List.length right_steps > 0 in
         let only_left_steps = List.length left_steps > 0 && List.length right_steps = 0 in 
         if steps_non_empty then
-            (printSteps left_steps) @ (printRightSteps right_steps) @ [Print.string_of_expr right ^ "\n"]
+            (printStepsWithDefinitions left_steps) @ (printRightSteps (List.rev right_steps)) @ [Print.string_of_expr right ^ "\n"]
+            (* (printStepsWithDefinitions left_steps) @ (List.rev(printStepsWithDefinitions right_steps)) @ [Print.string_of_expr right ^ "\n"] *)
         else if only_left_steps then
-            printSteps left_steps
+            printStepsWithDefinitions left_steps
         else
             ["= { ??? }\n" ^ Print.string_of_expr right ^ "\n"]
     | _ -> raise (SyntaxError "produceProof: Expected an equality")
 
 
-let processLemma (name, (def : Ast.expr), (hint_opt : Ast.hint option)) (lemma_list : (string * Ast.expr list option * Ast.expr) list) =
-    let processEquality e1 e2 hintDescription = 
+let generateVariables lst =
+    let rec aux acc i = function
+        | [] -> acc
+        | _::xs ->
+            let suffix = if i mod 2 = 0 then "h" else "t" in
+            let num = if i >= 2 then string_of_int (i / 2) else "" in
+            let new_acc = if acc = "" then suffix ^ num else acc ^ ", " ^ suffix ^ num in
+            aux new_acc (i + 1) xs
+    in
+    aux "" 0 lst
+    
+
+let rec getCaseList (induction_variable: string) (cons : Ast.expr list) : Ast.expr list =
+    match cons with
+    | Ast.Constructor (nm, expr_list) :: tail -> 
+        (match expr_list with
+        | None -> parse (induction_variable ^ " = " ^ nm) :: getCaseList induction_variable tail
+        | Some lst -> parse (induction_variable ^ " = " ^ nm ^ "(" ^ (generateVariables lst) ^ ")"):: getCaseList induction_variable tail)
+    | [] -> []
+    | _ -> raise (SyntaxError "getCaseList: Expected a constructor")
+
+
+let rec getCases (type_name : string) (variable : string) (type_entries : Ast.declaration list) = 
+    match type_entries with
+    | Ast.Type(nm, type_list) :: tail -> 
+        (if nm = type_name then getCaseList variable type_list
+        else getCases type_name variable tail)
+    | [] -> raise (SyntaxError "getCases: no corresponding type found")
+    | _ -> raise (SyntaxError "getCases: Expected a type")
+
+
+let generateIH (induction_variable : string) (leq : Ast.expr) (req : Ast.expr) = 
+    Substitution.apply (Substitution.singleton induction_variable (Ast.Id "t")) (parse (Print.string_of_expr leq ^ " = " ^ Print.string_of_expr req))
+
+let processLemma (name, (def : Ast.expr), (hint_opt : Ast.hint option)) (lemma_list : (string * Ast.expr list option * Ast.expr) list) (type_entries : Ast.declaration list) =
+    let processAxiom e1 e2 hintDescription = 
         name ^ " is assumed as an axiom:\n" ^ Print.string_of_expr e1 
         ^ "\n= { " ^ hintDescription ^ " }\n" ^ Print.string_of_expr e2 ^ "\n"
+    in
+    let rec appendCases (my_cases : Ast.expr list) (my_lemma_list : (string * Ast.expr list option * Ast.expr) list) =
+        match my_cases with
+        | h :: t -> 
+            let new_lemma = "case " ^ Print.string_of_expr h in 
+            my_lemma_list @ [(new_lemma, None, h)] @ appendCases t my_lemma_list
+        | [] -> my_lemma_list
+    in
+    let rec generateProofCase (my_cases : Ast.expr list) (left_eq : Ast.expr) (right_eq : Ast.expr) (my_lemma_list : (string * Ast.expr list option * Ast.expr) list) =
+        match my_cases with
+        | h :: t ->
+            "Case " ^ Print.string_of_expr h ^ ":\n" ^
+            Print.string_of_expr left_eq ^ "\n" ^ String.concat "" (produceProof (Ast.Equal(left_eq, right_eq)) (appendCases [h] my_lemma_list)) ^ "\n"
+            ^ generateProofCase t left_eq right_eq my_lemma_list
+        | [] -> ""
     in
     match def with
     | Ast.Equal (e1, e2) -> (
         match hint_opt with
-        | Some Axiom -> processEquality e1 e2 "axiom"
-        | Some (Induction x) -> processEquality e1 e2 (" is proved by induction on " ^ x ^ ":\n")
+        | Some Axiom -> processAxiom e1 e2 "axiom"
+        | Some (Induction x) -> 
+            (let cases = getCases "list" x type_entries in
+            (* let hypothesis = generateIH x e1 e2 in *)
+            (* let lemma_list_with_hypothesis = ("induction hypothesis", None, hypothesis) ::lemma_list in *)
+            "Proof of " ^ name ^ " by induction on " ^ x ^ ":\n" ^ generateProofCase cases e1 e2 lemma_list)
         | None -> "Proof of " ^ name ^ ":\n" ^ Print.string_of_expr e1 ^ "\n" ^ String.concat "" (produceProof def lemma_list)
     )
     | _ -> raise (SyntaxError "processLemma: Expected an equality")
@@ -268,37 +336,47 @@ let getFunction (name : string) (function_header : Ast.expr) (function_body : As
         parse (left_eq ^ " = " ^ right_string ^ formatMatchCases expr_list)
     | _ -> raise (SyntaxError "getFunction: Expected a match expression")
    
-    
+  
+(* Debug Function *)
+let rec print_lemma_list (lemma_list : (string * Ast.expr list option * Ast.expr) list) : string =
+    match lemma_list with
+    | [] -> ""
+    | (name, _, def) :: tail -> 
+        name ^ " : " ^ Print.string_of_expr def ^ "\n" ^ print_lemma_list tail
+
+
 let produce_output_simple (decls : Ast.declaration list) : string =
     (* Nested helper function to process each declaration *)
-    let rec processDecls decls acc =
+    let rec processDecls decls acc type_entries =
         match decls with
-        | [] -> "" (* Placeholder for final accumulator processing *)
+        | [] -> "" 
+                ^ "Warning: Not yet finished with induction hypotheses\n"
+                (* ^ print_lemma_list acc *)
         | h :: t ->
             match h with
             | Ast.Lemma(name, variables, def, hint_opt) ->
                 let lemma_entry = ("lemma " ^ name, variables, def) in
-                let result = processLemma (name, def, hint_opt) acc ^ "\n" in
-                result ^ processDecls t (acc @ [lemma_entry])
+                let result = processLemma (name, def, hint_opt) acc type_entries ^ "\n" in
+                result ^ processDecls t (acc @ [lemma_entry]) type_entries
   
-            | Ast.Type (_, _) ->
-                failwith "unimplemented"
+            | Ast.Type (nm, type_list) -> let type_entry = Ast.Type (nm, type_list) in
+                "" ^ processDecls t acc (type_entry::type_entries)
   
             | Ast.Function (function_header, function_body) ->
-                processFunction function_header function_body acc t
+                processFunction function_header function_body acc t type_entries
   
     (* Helper function to process function declarations *)
-    and processFunction function_header function_body acc t =
+    and processFunction function_header function_body acc t type_entries =
         match function_header with
         | Ast.FunctionSignature (name, _, variables) ->
             let function_records = getFunctionRec name function_header function_body in
             let new_acc_entries = List.map (fun record -> ("definition of " ^ name, Some variables, record)) function_records in
             (* let function_records = getFunction name function_header function_body in *)
             (* let new_acc_entries = [("definition of " ^ name, Some variables, function_records)] in *)
-            processDecls t (acc @ new_acc_entries)
+            processDecls t (acc @ new_acc_entries) type_entries
 
         | _ -> raise (SyntaxError "produce_output_simple: Expected a function signature")
     in
-    processDecls decls []
+    processDecls decls [] []
   
   
